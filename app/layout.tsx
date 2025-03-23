6b261b1f16208a6343e05e54bbaa117ee81be1ab
David Schalin <david.schalin00@gmail.com>
2025-03-23 17:35:24 +0100

    Strecka med flattis

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,10 +4,10 @@ import { Metadata } from 'next';
  
 export const metadata: Metadata = {
   title: {
-    template: '%s | Acme Dashboard',
-    default: 'Acme Dashboard',
+    template: '%s | NKM Dashboard',
+    default: 'NKM Dashboard',
   },
-  description: 'The official Next.js Learn Dashboard built with App Router.',
+  description: 'The ALPHA NKM Site.',
   metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
 };
 export default function RootLayout({
