1d51b2deaba2c5dbc6cf5f510e4161807522d973
David Schalin <david.schalin00@gmail.com>
2025-04-22 14:53:47 +0200

    now?

--- app/lib/gcs.ts
+++ app/lib/gcs.ts
@@ -5,14 +5,14 @@ import { writeFileSync } from 'fs';
 import path from 'path';
 
 export function getGCSClient() {
-  const keyPath = path.join(tmpdir(), 'gcs-key.json');
-
   if (!process.env.GCS_KEY_BASE64) throw new Error("Missing GCS_KEY_BASE64");
-  const jsonString = Buffer.from(process.env.GCS_KEY_BASE64, 'base64').toString('utf8');
-  writeFileSync(keyPath, jsonString);
+
+  const credentials = JSON.parse(
+    Buffer.from(process.env.GCS_KEY_BASE64, 'base64').toString('utf8')
+  );
 
   return new Storage({
     projectId: process.env.GCS_PROJECT_ID,
-    keyFilename: keyPath,
+    credentials, // <- safer and avoids fs I/O
   });
 }
