8adebf31a9482508ec75647f9a0a0582c0e22526
David Schalin <david.schalin00@gmail.com>
2025-06-07 18:07:52 +0200

    minor minor fix

--- app/ui/dashboard/saldo.tsx
+++ app/ui/dashboard/saldo.tsx
@@ -30,7 +30,7 @@ export default async function SaldoBox({ id, role }: { id: string, role: string}
     <div className="p-4 border rounded-lg shadow-md bg-surface-light dark:bg-surface-dark text-text-light dark:text-text-dark w-64 text-center">
       <p className="text-lg font-semibold">Strecka av dig till: </p>
       {/* <Link href={`https://app.swish.nu/1/p/sw/?sw=`+sn?.value+`&amt=200&cur=SEK&msg=Lista&edit=amt,msg&src=url`}> */}
-      <p className="text-lg text-primary-light font-semibold">{sn?.value ? sn.value : "Laddar..."}</p>
+      <p className="text-lg font-semibold">{sn?.value ? sn.value : "Laddar..."}</p>
       {/* </Link> */}
 
       <p className="text-lg font-semibold">Ditt Saldo:</p>
