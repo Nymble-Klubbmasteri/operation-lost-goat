97f3abfa277f84ebb57bb5af714f846d14649acb
antlilja <liljaanton2001@gmail.com>
2026-02-05 01:51:56 +0100

    Don't nest links

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -7,14 +7,9 @@ import { signOut } from '@/auth';
 export default async function SideNav({ role, admin }: { role: string, admin: string }) {
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
-      <Link
-        className="mb-2 flex h-20 rounded-md p-4 md:h-40"
-        href="/"
-      >
-        <div className="h-full w-full flex justify-center items-center">
-          <NKMLogo className="max-h-full w-auto object-contain" />
-        </div>
-      </Link>
+      <div className="mb-2 flex h-20 rounded-md p-4 md:h-40 justify-center items-center">
+        <NKMLogo className="max-h-full w-auto object-contain" />
+      </div>
       <div className="flex grow flex-row justify-between space-x-2 overflow-y-auto md:flex-col md:space-x-0 md:space-y-2">        <NavLinks role={role} admin={admin} />
         <div className="hidden h-auto w-full grow rounded-md bg-surface-light dark:bg-surface-dark md:block"></div>
         <form
