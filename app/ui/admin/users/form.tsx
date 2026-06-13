e21d5c62dd46fd44b6529351e30929f4cd2ada0e
David Schalin <david.schalin00@gmail.com>
2026-06-13 18:05:00 +0200

    Fixed admin edit users issue

--- app/ui/admin/users/form.tsx
+++ app/ui/admin/users/form.tsx
@@ -121,12 +121,11 @@ export default function Form({
               id="password"
               name="password"
               type="password"
-              defaultValue={user ? user.password : ""}
+              defaultValue={""}
               placeholder="Ange lösenord..."
               className="peer block w-full rounded-md border border-gray-200 dark:border-gray-600 bg-white dark:bg-gray-800 py-2 pl-10 text-sm outline-2 text-gray-900 dark:text-gray-100 placeholder:text-gray-500 dark:placeholder:text-gray-400"
               aria-describedby='password-error'
               minLength={6}
-              required
             />
             <LockClosedIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 dark:text-gray-400 peer-focus:text-gray-900 dark:peer-focus:text-gray-100" />
           </div>
